8efc4e6e0bc1eb428b5fdf6b58c9b748e8e1e6d3
Evan Crumpecker <evan@b7interactive.com>
2023-08-25 19:51:07 -0400

    add error boundary, modal using portals

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 const Details = () => {
 	const { id } = useParams();
+	const [showModal, setShowModal] = useState(false);
 	const results = useQuery(["details", id], fetchPet);
 
 	if (results.isLoading) {
@@ -25,11 +29,30 @@ const Details = () => {
 				<h2>
 					{pet.animal} - {pet.breed} - {pet.city} - {pet.state}
 				</h2>
-				<button>Adopt {pet.name}</button>
+				<button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
 				<p>{pet.description}</p>
+				{showModal ? (
+					<Modal>
+						<div>
+							<h1>Would you like to adopt {pet.name}?</h1>
+							<div className="buttons">
+								<button>Yes</button>
+								<button onClick={() => setShowModal(false)}>No</button>
+							</div>
+						</div>
+					</Modal>
+				) : null}
 			</div>
 		</div>
 	);
 };
 
-export default Details;
+function DetailsErrorBoundary(props) {
+	return (
+		<ErrorBoundary>
+			<Details {...props} />
+		</ErrorBoundary>
+	);
+}
+
+export default DetailsErrorBoundary;
