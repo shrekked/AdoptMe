ce17512c77097a3c47b55dbd73fe00cafcde6725
Evan Crumpecker <evan@b7interactive.com>
2023-08-28 11:09:29 -0400

    add context, styles, error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
@@ -14,16 +16,19 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+	const adoptedPet = useState(null);
 	return (
 		<BrowserRouter>
 			<QueryClientProvider client={queryClient}>
-				<header>
-					<Link to="/">Adopt me!</Link>
-				</header>
-				<Routes>
-					<Route path="/details/:id" element={<Details />} />
-					<Route path="/" element={<SearchParams />} />
-				</Routes>
+				<AdoptedPetContext.Provider value={adoptedPet}>
+					<header>
+						<Link to="/">Adopt me!</Link>
+					</header>
+					<Routes>
+						<Route path="/details/:id" element={<Details />} />
+						<Route path="/" element={<SearchParams />} />
+					</Routes>
+				</AdoptedPetContext.Provider>
 			</QueryClientProvider>
 		</BrowserRouter>
 	);
