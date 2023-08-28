01e0bf3a422943647c20f0278e9810e15ba12118
Evan Crumpecker <evan@b7interactive.com>
2023-08-28 12:51:15 -0400

    messing with exports..

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -2,8 +2,8 @@ import { useState, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
 import Results from "./Results";
-import useBreedList from "./useBreedList";
-import fetchSearch from "./fetchSearch";
+import fetchSearch from "./fetchSearch.js";
+import useBreeds from "./useBreeds.js";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -13,7 +13,7 @@ const SearchParams = () => {
 		breed: "",
 	});
 	const [animal, setAnimal] = useState("");
-	const [breeds] = useBreedList(animal);
+	const [breeds] = useBreeds(animal);
 	const [adoptedPet] = useContext(AdoptedPetContext);
 
 	const results = useQuery(["search", requestParams], fetchSearch);
--- src/useBreeds.js
+++ src/useBreeds.js
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import fetchBreedList from "./fetchBreedList";
 
-function useBreedList(animal) {
+function useBreeds(animal) {
 	const results = useQuery(["breeds", animal], fetchBreedList);
 
 	return [results?.data?.breeds ?? [], results.status];
 }
 
-export default useBreedList;
+export default useBreeds;
