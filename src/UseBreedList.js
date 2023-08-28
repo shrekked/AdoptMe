991cb902c20a013d58ab8a353472ab0f92f80262
Evan Crumpecker <evan@b7interactive.com>
2023-08-28 12:28:36 -0400

    update useBreedList export

--- src/UseBreedList.js
+++ src/UseBreedList.js
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import fetchBreedList from "./fetchBreedList";
 
-export default function useBreedList(animal) {
+function useBreedList(animal) {
 	const results = useQuery(["breeds", animal], fetchBreedList);
 
 	return [results?.data?.breeds ?? [], results.status];
 }
+
+export default useBreedList;
